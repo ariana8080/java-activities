739d114528f3fe693b427634bb5608be426460a0
samanthadaphnet <sdytayactac@gmail.com>
2024-12-15 16:44:57 +0800

    Task 113 simple assignment operator

---
+++ Tasksheet113.java
@@ -0,0 +1,27 @@
+public class Tasksheet113 {
+    public static void main(String[] args) {
+
+        int result = 1 + 2;
+        System.out.println(result);
+        // result is 3
+
+        result -= 1;
+        System.out.println(result);
+        // result is 2
+
+        result *= 2;
+        System.out.println(result);
+        // result is 4
+
+        result /= 2;
+        System.out.println(result);
+        // result is 2
+
+        result += 8;
+        result %= 7;
+        System.out.println(result);
+        // result is 10
+        // result is 3
+}
+    
+}
